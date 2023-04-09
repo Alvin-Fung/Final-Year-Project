152ed85d6e88eb6689ac16dc7b5e4b0d16e1e690
Alvin-Fung <alvin_fung9@outlook.com>
2023-04-09 13:05:35 +0100

    Rewrite of FYP
    My understanding of Python as of this moment is a lot stronger than Java. Seems to be a lot more approachable methods with Python.

---
+++ rewrite.py
@@ -0,0 +1,16 @@
+#A function to load notes
+
+#A function to load chords
+
+
+
+
+
+#Producer of notes and chords
+
+#Consumer - continously looping waits for chords and notes to play
+
+
+
+
+#Later on: note pattern generator, keeps track of where it is(state) within the harmonic section 
