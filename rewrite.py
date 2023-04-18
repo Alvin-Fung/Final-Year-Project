ed96e0db5bd12f6747583fb16a7b953453bc9c03
Alvin-Fung <alvin_fung9@outlook.com>
2023-04-18 15:13:57 +0100

    Rewrote the previous logic used in the first iteration of the project

--- rewrite.py
+++ rewrite.py
@@ -1,19 +1,67 @@
 import os, os.path
-import numpy as np
 from playsound import playsound
+import random
+random.random
+import threading
+import time
 
-notePath = os.listdir("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/A Natural Minor Scale")
-notesArray = np.array(notePath)
-print("Notes: ", notesArray)
 
-chordPath = os.listdir("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/Chords")
-chordArray = np.array(chordPath)
-print("Chords: ",chordArray)
+def loadPath(rootPath):
+    noteFileNames = os.listdir(rootPath)
+    notePaths = []
 
-playsound('D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/Chords/CMaj7.mp3')
+    for noteFileName in noteFileNames:
+        fullPath = os.path.join(rootPath, noteFileName)
+        notePaths.append(fullPath)
+
+    return notePaths
+
+notePaths = loadPath("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/A Natural Minor Scale")
+chordPaths = loadPath("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/Chords")
+
+def playSound(path):
+    playsound(path)
 
 #Producer of notes and chords
+def noteOrder(notePaths):
+    pass
 
-#Consumer - continously looping waits for chords and notes to play
+currentChord = 0
+checkChord = 0
+currentNote = 0
 
+def nextChord(chordPaths):
+    global currentChord, checkChord
+    checkChord += 1
+
+    if (checkChord % 4 == 0):
+        currentChord += 1
+        if(currentChord == len(chordPaths)):
+            currentChord = 0
+
+    return chordPaths[currentChord]
+
+def nextNote(notePaths):
+    global currentNote
+    if (currentNote == len(notePaths) & 16):
+        currentNote = 0
+    else:
+        currentNote += 1
+        # currentNote = random.randint(0,16)
+        print(notePaths[currentNote].split("\\")[1])
+    return notePaths[currentNote]
+    
+
+def threadFunction():
+    pass
+
+def play():
+    while True:
+            chord = nextChord(chordPaths)
+            note = nextNote(notePaths)
+            playSound(chord, False)
+            playSound(note)
+play()
+
+#Consumer - continously looping waits for chords and notes to play (await.function())
 #Later on: note pattern generator, keeps track of where it is(state) within the harmonic section
