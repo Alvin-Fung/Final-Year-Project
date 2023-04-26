c3fe299c3beeb38fc6333cd0129f6e55a4e0c0d0
Alvin-Fung <alvin_fung9@outlook.com>
2023-04-26 19:17:17 +0100

    Integrated noteSequencer logic into nextNote() for now so that I can see when a pattern can be used

--- rewrite.py
+++ rewrite.py
@@ -6,6 +6,8 @@ random.random
 import threading
 import time
 
+def playSound(path, x):
+    playsound(path, x)
 
 def loadPath(rootPath):
     noteFileNames = os.listdir(rootPath)
@@ -20,11 +22,13 @@ def loadPath(rootPath):
 notePaths = loadPath("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/A Natural Minor Scale")
 chordPaths = loadPath("D:/Stuff/Programming/TYP/Personal-Code/MusicFiles/Chords")
 
-def playSound(path, x):
-    playsound(path, x)
-
 #Producer of notes and chords
-currentChord, currentNote, checkChord = 0,0,0
+patternMap = {
+        "0": [4,6,4,9,7],
+        "1": [3,5,7,8,9,11,10,9]
+        # More patterns to be made later etc.
+    }
+currentChord, currentNote, currentPattern, checkChord = 0,0,0,0
 
 def nextChord(chordPaths):
     global currentChord, checkChord
@@ -38,66 +42,39 @@ def nextChord(chordPaths):
     print(chordPaths[currentChord].split("\\")[1])
     return chordPaths[currentChord]
 
-def nextNote(notePaths):
-    global currentNote
-    if (currentNote == len(notePaths) & 16):
-        currentNote = 0
-    else:
-        currentNote += 1
-        # currentNote = random.randint(0,16)
-        print(notePaths[currentNote].split("\\")[1])
-        print("========")
-    return notePaths[currentNote]
-    
-def noteSequencer(currentNote, currentPattern):
-    
-    patternMap = {
-        "0": [4,6,4,9,7],
-        "1": [3,5,7,8,9,11,10,9]
-        # More patterns to be made later etc.
-    }
+def nextNote(notePaths, patternMap):
+    global currentNote,currentPattern
     
     while True:
-        if (currentPattern >=len(patternMap[str(currentNote)])):
-            if(currentNote == 2):
-                currentNote = "0"
-                print("Starting Pattern One...")
-                currentPattern = 0
-            elif (currentNote == 3):
-                currentNote = "1"
-                currentPattern = 0
-                print("Starting Pattern Two...")
+            if (currentPattern >=len(str(currentNote))):
+                if(currentNote == 2):
+                    currentNote = patternMap["0"]
+                    print("Starting Pattern One...")
+                    currentPattern = 0
+                elif (currentNote == 3):
+                    currentNote = patternMap["1"]
+                    currentPattern = 0
+                    print("Starting Pattern Two...")
+                else:
+                    currentPattern += 1
+                    print("Switching back to Note", currentNote) 
+                    
+                    return currentPattern
+
+            if (currentNote == len(notePaths) & 16):
+                currentNote = 0
             else:
-                currentPattern += 1
-                currentPattern = 0
-                print("Switching back to Note", currentNote) 
-                
-            return currentNote, currentPattern
+                currentNote += 1
+                currentNote = random.randint(0,16)
+                print(notePaths[currentNote].split("\\")[1])
+                print("========")
+                return notePaths[currentNote]
 
-
-def consumer():
-    #Also acquiring set of notes to be then played  
-    global notePaths
-    currentChord,currentPattern,currentNote = 0,0
-    #Keeps track of the current chord and note being played
-    currentNotePath = notePaths[currentNote]
-    currentChordPath = chordPaths[currentChord] 
-    
-    #Play the notes at a time
-    while True:
-        currentNotePath = nextNote(notePaths) #Allows for next note progression
-        currentPattern = noteSequencer(currentNotePath) #Then utilise the noteSequencer for patterns.
-        #Have some sort set of rules/random generation.
-        time.sleep(5)
-        yield currentPattern
- 
-    #Updating what the current note and chord is and sends the results to play() function below.
-   
 def play():
     while True:
+        #Have conversion from index position to an actual note here
         chord = nextChord(chordPaths)
-        note = consumer()
+        note = nextNote(notePaths, patternMap)
         playSound(chord, False)
-        playSound(note)        
+        playSound(note, True)        
 play()
-
